50f309a6420d363bfc6c4dabcf247f0a93b95abf
Janne <janneonkiva@gmail.com>
2020-01-28 21:28:39 +0200

    kommitti

---
+++ app/src/main/java/com/example/a3assignment/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.a3assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonAddTime;
+    EditText text;
+    ListView listView;
+    Calendar calendar = Calendar.getInstance();
+    String currentDate = DateFormat.getDateInstance().format(calendar.getTime());
+    ArrayList<Calendar> arrayList = new ArrayList<Calendar>();
+    ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, arrayList);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonAddTime = (Button) findViewById(R.id.buttonAddTime);
+        buttonAddTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+        });
+        listView=(ListView)findViewById(R.id.listview);
+
+        final ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add(currentDate);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, arrayList);
+
+        listView.setAdapter(arrayAdapter);
+
+
+    }
+
+    public void openDialog() {
+        OpenDialog openDialog = new OpenDialog();
+        openDialog.show(getSupportFragmentManager(), "open dialog");
+    }
+}
+
